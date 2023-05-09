5089af6e64e05d00678348ec78c30ae4a13bf017
David Bolt <David.Bolt@Sfcknox.org>
2023-05-09 19:27:11 -0400

    work on contacts

--- contacts/contacts.js
+++ contacts/contacts.js
@@ -2,3 +2,6 @@ import  {contactsModule}   from '/synergyData/contacts/contactsModule.js';
 
 
 app.contacts = new contactsModule();
+
+//get ride of footer
+document.getElementById("footer"    ).innerHTML = "";
