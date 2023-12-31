b38c322c343c4774a47217945ebf760a131d57a6
David Bolt <David.Bolt@Sfcknox.org>
2023-12-31 12:25:15 -0500

    change parameter to calendar main to year from url

--- calendar/page_module.js
+++ calendar/page_module.js
@@ -1,4 +1,3 @@
 import  {calendarClass  }   from '/_lib/UX/calendar_module.js'  ;
-
 app.page = new calendarClass("weeks","app.page");
-app.page.main(`/users/events/${app.page.year}/_graph.json` );
+app.page.main(app.page.year);
