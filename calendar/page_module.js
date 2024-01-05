0776084641a8dbe0d2b7f92bc45f1bfaf271776d
David Bolt <David.Bolt@sfcknox.org>
2024-01-05 09:14:34 -0500

    calender_add - new method

--- calendar/page_module.js
+++ calendar/page_module.js
@@ -1,4 +1,5 @@
 import  {calendarClass  }   from '/_lib/UX/calendar_module.js';
 
 app.page = new calendarClass("weeks","app.page");
+app.page.calender_add("users/databases/synergy/calendar");
 await app.page.main(new Date().getFullYear()); 
