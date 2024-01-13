c1a2ccdee9c50c68775e7704530a0e64f69247b5
David Bolt <David.Bolt@sfcknox.org>
2024-01-13 08:31:11 -0500

    refactor

--- calendar/page_module.js
+++ calendar/page_module.js
@@ -1,6 +1,6 @@
 import  {calendarClass  }   from '/_lib/UX/calendar_module.js';
 
-app.page = new calendarClass("weeks","app.page");
-app.page.calender_add("users/databases/synergy/calendar");
-await app.page.init();                            // load calenders and create html place holders
-await app.page.main(new Date().getFullYear()); 
+      app.page = new calendarClass("weeks","app.page");
+      app.page.calender_add("users/databases/synergy/calendar");  
+await app.page.init();                                             // load calenders and create html place holders
+await app.page.main();                                             // display calender or event page
