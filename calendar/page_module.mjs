50611dc722f705cdfd555e1b1684385811902021
David Bolt <David.Bolt@Sfcknox.org>
2024-07-18 16:06:37 -0400

    commit out line of code

--- calendar/page_module.mjs
+++ calendar/page_module.mjs
@@ -4,5 +4,3 @@ import  {calendar_class}    from '/_lib/web_componets/calendar-sfc/_.mjs';  // s
       app.page.calendar_add("users/databases/synergy/calendar");   // get users personal calendar, must be logged in
 await app.page.init();                                             // load calenders and create html place holders
 await app.page.main();                                             // display calender or event page
-//app.page.tableUx.changePageSize({value: 5});
-
